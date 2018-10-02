a7fce7bbe12363dbf98945b418522cd2b6fed0d3
Cricky Cicchetti <cricky.cicchetti@gmail.com>
2018-10-02 00:09:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ let feet = 200;
 
 function scuberGreetingForFeet(feet) {
   if (feet <= 400) {
-    return "This one is on me!"
+    return 'This one is on me!'
   } else if (feet > 2000){
-    return "I will gladly take your thirty bucks."
+    return 'I will gladly take your thirty bucks.'
   }
   else if (feet > 2500) {
-    return "I will gladly take your thirty bucks."
+    return 'No can do.'
   } else if (feet = )
 }
