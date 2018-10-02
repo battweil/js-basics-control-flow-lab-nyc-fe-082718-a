aaddccde587538fc36bc20b5081757b4105854f5
Cricky Cicchetti <cricky.cicchetti@gmail.com>
2018-10-02 00:02:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+let feet = 200;
+
+function scuberGreetingForFeet(feet) {
+  if (feet > 0) {
+    return "This one is on me!"
+  } else {
+    return "NOT positive";
+  }
+}
+
+console.log(testNum(-5));
